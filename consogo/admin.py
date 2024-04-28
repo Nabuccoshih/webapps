3a069aff4dbbd247cbaa08d4a363d5091e73cf64
Nabuccoshih <63971042+Nabuccoshih@users.noreply.github.com>
2024-04-28 16:22:39 +0000

    affinage de l'affichage dans l'admin et insertion d'un calcul

--- consogo/admin.py
+++ consogo/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
+
 from .models import Conso
+
+## calcul conso => (litres de carburant X 100) / Kilomètres parcourus
 
 
 class ConsoAdmin(admin.ModelAdmin):
-	list_display = ['date', 'kilometrage', 'prix_au_litre', 'volume']
+	list_display = [
+	    'date', 'kilometrage', 'prix_au_litre', 'volume',
+	    'calculate_total_cost'
+	]
+
+	def calculate_total_cost(self, obj):
+		if obj.kilometrage and obj.prix_au_litre and obj.volume:
+			return "{:.2f}".format(((obj.volume * 100) / obj.kilometrage))
+		else:
+			return None
+
+	calculate_total_cost.short_description = 'Conso. Moy.'
 
 
 admin.site.register(Conso, ConsoAdmin)
--- consogo/models.py
+++ consogo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Conso(models.Model):
 	date = models.DateField()
 	kilometrage = models.IntegerField()
-	prix_au_litre = models.DecimalField(max_digits=3, decimal_places=2)
+	prix_au_litre = models.DecimalField(max_digits=4, decimal_places=3)
 	volume = models.DecimalField(max_digits=4, decimal_places=2)
 
 	def __str__(self):
