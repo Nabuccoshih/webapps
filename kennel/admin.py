42db2b7640f4eb2f1b78dff89b7d915ead248dba
Nabuccoshih <63971042+Nabuccoshih@users.noreply.github.com>
2024-05-05 15:53:46 +0000

    Ajout des tables chiot, client et portée.

--- kennel/admin.py
+++ kennel/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from django.utils import timezone
 
-from .models import Chien, Affixe
+from .models import Chien, Affixe, Chiot, Client, Portee
 
 
 class ChienAdmin(admin.ModelAdmin):
@@ -14,5 +15,23 @@ class AffixeAdmin(admin.ModelAdmin):
 	list_display = ['nom']
 
 
+class ChiotAdmin(admin.ModelAdmin):
+	list_display = ['nom', 'sexe', 'couleur']
+
+
+class PorteeAdmin(admin.ModelAdmin):
+	list_display = ['nom', 'dob', 'pere', 'mere']
+
+
+class ClientAdmin(admin.ModelAdmin):
+	list_display = [
+	    'nom', 'prenom', 'adresse', 'cp', 'ville', 'tel', 'email', 'dobc',
+	    'chiot_choisi'
+	]
+
+
 admin.site.register(Chien, ChienAdmin)
 admin.site.register(Affixe, AffixeAdmin)
+admin.site.register(Chiot, ChiotAdmin)
+admin.site.register(Portee, PorteeAdmin)
+admin.site.register(Client, ClientAdmin)
--- kennel/models.py
+++ kennel/models.py
@@ -1,8 +1,16 @@
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, EmailValidator
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 from dateutil.relativedelta import relativedelta
+
+SEXE_CHOICES = (
+    ('M', 'Mâle'),
+    ('F', 'Femelle'),
+)
+
+COLOR_CHOICES = (('N', 'Noir'), ('NB', 'Noir et Blanc'), ('TC', 'Tricolore'),
+                 ('DB', 'Doré et Blanc'))
 
 
 class Affixe(models.Model):
@@ -18,14 +26,6 @@ class Chien(models.Model):
 		if len(str(value)) != 18:
 			raise ValidationError(
 			    'Le champ puce doit contenir exactement 18 caractères.')
-
-	SEXE_CHOICES = (
-	    ('M', 'Mâle'),
-	    ('F', 'Femelle'),
-	)
-
-	COLOR_CHOICES = (('N', 'Noir'), ('NB', 'Noir et Blanc'),
-	                 ('TC', 'Tricolore'), ('DB', 'Doré et Blanc'))
 
 	nom = models.CharField(max_length=255)
 	affixe = models.ForeignKey(Affixe, on_delete=models.CASCADE, default=None)
@@ -59,3 +59,62 @@ class Chien(models.Model):
 			return None
 
 	age.short_description = 'Âge'
+
+
+class Chiot(models.Model):
+	nom = models.CharField(max_length=50)
+	sexe = models.CharField(max_length=1, choices=SEXE_CHOICES, default='M')
+	couleur = models.CharField(max_length=2,
+	                           choices=COLOR_CHOICES,
+	                           default='TC')
+
+	def __str__(self):
+		return str(self.nom)
+
+
+class Client(models.Model):
+
+	def validate_cp(value):
+		if len(str(value)) != 5:
+			raise ValidationError(
+			    'Le code postal doit contenir exactement 5 caractères numériques.'
+			)
+
+	def validate_tel(value):
+		phone_regex = RegexValidator(
+		    regex=r'^(\+\d{2,3})? ?(\d{2} ?){4}\d{2}$',
+		    message=
+		    'Le numéro de téléphone doit être au format "01 23 45 67 89".')
+		phone_regex(value)
+
+	nom = models.CharField(max_length=50)
+	prenom = models.CharField(max_length=50)
+	adresse = models.CharField(max_length=255)
+	cp = models.CharField(max_length=5, validators=[validate_cp])
+	ville = models.CharField(max_length=50)
+	tel = models.CharField(max_length=14, validators=[validate_tel])
+	email = models.EmailField(validators=[EmailValidator()])
+	dobc = models.DateField()
+	chiot_choisi = models.ForeignKey(Chiot,
+	                                 on_delete=models.CASCADE,
+	                                 default=None)
+
+	def __str__(self):
+		return str(self.nom)
+
+
+class Portee(models.Model):
+	nom = models.CharField(max_length=50)
+	dob = models.DateField()
+	pere = models.ForeignKey(Chien,
+	                         related_name='portees_pere',
+	                         on_delete=models.CASCADE,
+	                         default=None)
+	mere = models.ForeignKey(Chien,
+	                         related_name='portees_mere',
+	                         on_delete=models.CASCADE,
+	                         default=None)
+	chiot = models.ManyToManyField(Chiot)
+
+	def __str__(self):
+		return str(self.nom)
